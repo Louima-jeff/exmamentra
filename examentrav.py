dd3a6b8740a941d8167f46d7bf1049de823c3206
Louima-jeff <GEFMYLOUIMA1986@GMAIL.COM>
2025-07-11 21:02:02 -0400

    Add files via upload

---
+++ examentrav.py
@@ -0,0 +1,42 @@
+# productos={ modelo:[marca, pantalla, Ram,disco, de DD, procesador,video],...}
+
+
+productos={'8475HD':['Hp',15.6,'8GB','DD','1T','Intel Corei5','NvidaGTX1050'],
+'2175HD':['lenovo',14,'4GB','SSD','512GB','Intel Corei5','NvidiaGTX1050'],
+'JjfFHD':['Asus',14, '16GB','SSD','256GB','Intelcorei7','NvidiaRTX2080Ti'],
+'fgdxFHD':['HP',15.6,'8GB','DD','1T','IntelCorei3','integrada'],
+ 'GF75HD':['Asus','15.6,''8GB','DD','1T','Intelcorei7','NvidiaGTX1050'],
+ '123FHD':['lenovo','14,''6GB','DD,''1T,''AMDRyzen5','integral'],
+ '342FHD':['lenovo','15.6','8GB','DD',"1T", 'AMDRzen7','NvidiaGTX1050'],
+ "UWU131HD":["Dell","15.6""8GB","DD","1T",'AMDRzen3','NvidiaGTX1050'],            }
+
+#stocks={modelo:[precio,stocks],...]
+stocks={"8475HD":[387990],"2175HD":[327990],"JjfFHd":[424990,1],
+        "fgdxFHD":[664990,21],"123FHD"":[290890,32],""342FHD":[444990,7],
+        "GF75HD":[7499990,2],"UWU131HD":[349990,1],"FS1230HD":[249990,0],}
+        
+
+***Menu principal***
+def stock marcas():
+marca=productos.valuees():
+    encontrado=[]
+    for marca in marcas :
+    marca a_consulta=marca[0]
+    if marca a _consultado .lower()=marcalower()
+    encontrado.oppend(marca)=0
+    returnencontrado=[]
+    for modelo.oppend(productos[marca]):
+    if modelo.lower()=modelo.lower():
+    encontrado.append(modelo)
+returnencontrado
+                           
+                        
+
+
+    
+
+                    
+    
+
+
+
